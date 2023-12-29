a26887255295059f57f4903d34af1e129281611d
eduard <eduardcamayo13@gmail.com>
2023-12-29 12:16:30 -0500

    Desarrollando sección de la app Pt2

--- frontend/src/componentes/appCTM/AppCTM.tsx
+++ frontend/src/componentes/appCTM/AppCTM.tsx
@@ -1,51 +1,53 @@
+import Caracteristica from "./Caracteristica";
+const appCTM = "https://www.cootransmede.com/wp-content/uploads/2023/12/IMG_20231229_111616.jpg"
+
 const AppCTM = () => {
     return (
-        <div>
-            <div>
-                <img
-                    src="https://www.cootransmede.com/wp-content/uploads/2020/05/Phone.fw_.png"
-                    alt=""
-                />
-            </div>
-            <div>
-                <h1>Descargue nuestra APP cootransmede</h1>
-                <h4>
-                    Solicite su taxi desde su smatrphone en tan sólo unos
-                    segundos
-                </h4>
-                <p>
-                    Nuestra aplicación es fácil de usar y muy amigable. Elija
-                    ubicación de recogida, acepte el TAXI asignado y en pocos
-                    minutos estaremos en su puerta.
-                </p>
-            </div>
-            <div>
-                <h2>Características</h2>
-                <div>
-                    <span>1</span>
-                    <div>
-                        <h3>Funcionamiento en tiempo real.</h3>
-                        <p>Tiempo de respuesta en tiempo real con la centraL</p>
-                    </div>
+        <div className="flex items-center justify-center bg-[#f1f1f1]">
+            <div className="max-w-6xl py-6 md:flex md:px-5 md:py-8 font-raleway lg:gap-0">
+                <div className=" flex items-center justify-center w-full md:w-80 md:items-start">
+                    <a
+                        style={{backgroundImage: `url(${appCTM})`}}
+                        href=""
+                        className="bg-contain w-48 h-64 bg-no-repeat bg-center md:bg-top md:h-full lg:bg-center"
+                    ></a>
                 </div>
-                <div>
-                    <span>2</span>
-                    <div>
-                        <h3>Disponibilidad.</h3>
-                        <p>
-                            Comprueba los taxistas disponibles directamente
-                            desde la central.
+                <div className="flex flex-col items-center justify-center gap-10 max-w-3xl">
+                    <div className="flex flex-col px-3 gap-4">
+                        <h1 className="text-[#23A455] text-4xl flex font-bold xl:text-[40px]">
+                            Descargue nuestra APP cootransmede
+                        </h1>
+                        <h4 className=" flex font-semibold text-lg leading-none">
+                            Solicite su taxi desde su smatrphone en tan sólo
+                            unos segundos
+                        </h4>
+                        <p className="flex text-justify">
+                            Nuestra aplicación es fácil de usar y muy amigable.
+                            Elija ubicación de recogida, acepte el TAXI asignado
+                            y en pocos minutos estaremos en su puerta.
                         </p>
                     </div>
-                </div>
-                <div>
-                    <span>3</span>
-                    <div>
-                        <h3>Seguridad.</h3>
-                        <p>
-                            Conectándose directamente con la central reserva un
-                            coductos confiable y seguro para usted.
-                        </p>
+                    <div className="px-3 flex flex-col gap-3">
+                        <h2 className="text-[#23A455] text-3xl font-bold">
+                            Características
+                        </h2>
+                        <Caracteristica
+                            numero="1"
+                            subtitulo="Funcionamiento en tiempo real."
+                            parrafo="Tiempo de respuesta en tiempo real con la central."
+                        />
+                        <Caracteristica
+                            numero="2"
+                            subtitulo="Disponibilidad."
+                            parrafo="Comprueba los taxistas disponibles directamente
+                                desde la central."
+                        />
+                        <Caracteristica
+                            numero="3"
+                            subtitulo="Seguridad."
+                            parrafo="Conectándose directamente con la central reserva
+                                un coductos confiable y seguro para usted."
+                        />
                     </div>
                 </div>
             </div>
---
+++ frontend/src/componentes/appCTM/Caracteristica.tsx
@@ -0,0 +1,20 @@
+import { Caracteristica } from "../../interfaces/caracteristicas";
+
+const Caracteristica = ({numero, subtitulo, parrafo}: Caracteristica) => {
+    return (
+        <div className="flex items-start gap-2">
+            <span className="bg-[#23A455] text-center rounded-full px-[9px] py-[3px] text-[15px] text-white md:px-[13px] md:py-[6px]">
+                {numero}
+            </span>
+            <div>
+                <h3 className="text-[#23A455] text-lg">
+                {subtitulo}
+                    
+                </h3>
+                <p>{parrafo}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Caracteristica;
