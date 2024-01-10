534c30a29248b9bc39c3fa97f3247553532fc0df
eduard <eduardcamayo13@gmail.com>
2024-01-10 16:59:49 -0500

    Desarrollando la página de pagos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import Footer from "./componentes/footer/Footer";
 import Header from "./componentes/header/Header";
 import HeaderTop from "./componentes/header/HeaderTop";
-import Inicio from "./paginas/Inicio";
+import Pagos from "./paginas/Pagos";
 
 const App = () => {
     return (
         <>
             <HeaderTop />
             <Header /> 
-            <Inicio/>
+            <Pagos/>
             <Footer />
         </>
     );
