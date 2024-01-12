ea0acb4a2a3587a9506b787536d53677a2c08d85
eduard <eduardcamayo13@gmail.com>
2024-01-12 11:13:15 -0500

    Desarrollando Página de Contáctenos Pt2

--- frontend/src/componentes/Global/Input.tsx
+++ frontend/src/componentes/Global/Input.tsx
@@ -4,7 +4,7 @@ const Input = ({type="text", texto, required=false, id, placeholder}: Input) => 
     return (
         <div className="flex flex-col mb-3">
             <label htmlFor={id} className="text-white">{texto}</label>
-            <input type={type} required={required} id={id} placeholder={placeholder} className="py-2 px-2 text-[#7a7a7a] opacity-70 outline-none rounded"/>
+            <input type={type} required={required} id={id} placeholder={placeholder} className="py-1 px-2 text-[#7a7a7a] opacity-70 outline-none rounded"/>
         </div>
     );
 };
--- frontend/src/paginas/Contactenos.tsx
+++ frontend/src/paginas/Contactenos.tsx
@@ -7,17 +7,17 @@ const Contactenos = () => {
             style={{
                 backgroundImage: `url(https://www.cootransmede.com/wp-content/uploads/2020/10/ASAMBLEA-2-1024x683.jpg)`,
             }}
-            className=" relative  bg-no-repeat bg-cover bg-center"
+            className=" relative  bg-no-repeat bg-cover bg-center lg:flex lg:flex-col lg:items-center lg:justify-between"
         >
             <div className="bg-black absolute h-full w-full  opacity-60"></div>
 
-            <h1 className="text-white text-3xl font-semibold relative pl-4 py-4">
-                Contáctenos
-            </h1>
-
-            <div className="relative flex flex-col gap-4">
-                <div className="text-white px-4">
-                    <p>Oficina Administrativa</p>
+            <div className="relative flex flex-col w-full gap-4 py-8 max-w-[1024px] lg:flex-row lg:items-start lg:py-20">
+                <div className="text-white px-4 md:px-8">
+                    <h1 className="text-white text-3xl font-semibold relative pb-6 ">
+                        Contáctenos
+                    </h1>
+                    <span className="absolute h-[2px] w-20  bg-[#23A455] "></span>
+                    <p className="mt-3">Oficina Administrativa</p>
                     <p>Dirección: Calle 25#65B-48</p>
                     <p>Teléfono: 604 265 96 92</p>
                     <p>Correo: ctmgeneral@ctmcootransmede.com</p>
@@ -26,10 +26,10 @@ const Contactenos = () => {
                     <p>Contacto para solicitar servicio de Taxi:</p>
                     <p>Línea de Solicitud de Servicios: 604 265 65 65</p>
                     <p>Línea Única de Acopios: 604 322 0707</p>
-                    <p>Línea de WhatsApp 312 265 65 65</p>
+                    <p className="mb-3">Línea de WhatsApp 312 265 65 65</p>
+                    <span className="absolute h-[2px] w-20 bg-[#23A455] "></span>
                 </div>
-                {/* me puedes pasar este codigo para que funcione en react */}
-                <form action="" className="px-4 py-4 ">
+                <form action="" className="px-4 py-4 w-full max-w-lg mx-auto">
                     <Input
                         texto="Nombre"
                         required
@@ -50,7 +50,12 @@ const Contactenos = () => {
                         texto="Mensaje"
                         placeholder="Ingrese un Mensaje"
                     />
-                    <button type="submit" className="px-3 py-1 bg-[#23A455] text-white rounded-sm font-semibold">ENVIAR</button>
+                    <button
+                        type="submit"
+                        className="px-3 py-1 bg-[#23A455] text-white rounded-sm font-semibold"
+                    >
+                        ENVIAR
+                    </button>
                 </form>
             </div>
         </div>
